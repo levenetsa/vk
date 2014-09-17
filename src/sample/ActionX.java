7dff4f0ac7303ad22d50f0cc7fa5bc9ee71fe23e
levenetsa <nike240595@gmail.com>
2014-09-18 03:18:32 +0400

    well done

--- src/sample/ActionX.java
+++ src/sample/ActionX.java
@@ -4,13 +4,21 @@ import com.sun.xml.internal.ws.api.client.SelectOptimalEncodingFeature;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
+import java.lang.reflect.Constructor;
 
 /**
  * Created by ACER-PC on 17.09.2014.
  */
 public class ActionX extends AbstractAction {
-    @Override
+    String text1;
+    String text2;
+    public  ActionX(String s,String s1) {
+        text1=s;
+        text2=s1;
+    }
+
     public void actionPerformed(ActionEvent e) {
-        System.out.println("lol");
+
+        System.out.println();
     }
 }
--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,59 +1,102 @@
 package sample;
 
 
-import javafx.embed.swing.JFXPanel;
-import javafx.event.EventHandler;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
-import java.util.Scanner;
 
 public class Controller {
     static int counter = 1;
 
-    public static void save(String link) throws IOException {
-        if (counter>50){URL website = new URL(link);
-        ReadableByteChannel rbc = Channels.newChannel(website.openStream());
-        FileOutputStream fos = new FileOutputStream("information" + counter + ".mp3");
-        fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);}
+    public static void save(String link,String s) throws IOException {
+
+            URL website = new URL(link);
+            ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+            FileOutputStream fos = new FileOutputStream(s+"information" + counter + ".mp3");
+            fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+
         counter++;
     }
-    public static void draw(){
-        JFrame frame=new JFrame();
-        frame.setSize(600, 400);
+
+    public static String s1 = "1", s2 = "2";
+
+    public static void draw() {
+        final JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+        frame.setSize(550, 400);
         frame.setVisible(true);
-        JButton btn = new JButton("Go");
-        btn.setSize(80,40);
-        btn.setLocation(0, 0);
-        btn.setAction(action);
-        frame.add(btn);
-    }
+        final JButton button = new JButton();
+        button.setText("go");
+        button.setLocation(new Point(80, 40));
+        button.setPreferredSize(new Dimension(80, 40));
+        final JTextField field1 = new JTextField();
+        field1.setPreferredSize(new Dimension(500, 20));
+        final JTextField field2 = new JTextField();
+        final JLabel label3=new JLabel("enter folders");
+        ActionListener actL = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (e.getSource() instanceof JButton) {
+                    s1 = field1.getText();
+                    s2 = field2.getText();
+                   // System.out.println(s1 + " " + s2);
+                    try {
+                        File file = new File(s1);
+                        BufferedReader br = null;
 
-    public static Action action = new ActionX();
-
-    public static void main(String[] args) throws IOException {
-       /* File file = new File("E:/input.txt");
-        BufferedReader br = new BufferedReader(
-                new InputStreamReader(
-                        new FileInputStream(file), "UTF-8"));
+                        br = new BufferedReader(
+                                new InputStreamReader(
+                                        new FileInputStream(file), "UTF-8"));
 
 
-        String c = new String("a");
-        while (c != null) {
+                        String c = new String("a");
+                       int counter1=0;
 
-            if (c.contains("audios/")) {
-                save((String) c.subSequence(c.indexOf("http"), c.length() - 5));
+                        label3.setText("wait");
+                        while (c != null) {
+
+                            if (c.contains("audios/")) {
+                                save((String) c.subSequence(c.indexOf("http"), c.length() - 5),s2);
+
+                                counter1++;
+
+                            }
+                            c = br.readLine();
+                        }
+                        label3.setText("done");
+                    } catch (Exception e1) {
+
+                    }
+                }
+
 
             }
-            c = br.readLine();
-        }*/
+        };
+        button.addActionListener(actL);
+        field2.setPreferredSize(new Dimension(500, 20));
+        JLabel label1 = new JLabel("Input  ");
+        JLabel label2 = new JLabel("  Layout");
+        panel.setVisible(true);
+        panel.add(label1);
+        panel.add(field1);
+        panel.add(label2);
+        panel.add(field2);
+        panel.add(button);
+        panel.add(label3);
+        frame.getContentPane().add(panel);
+
+
+    }
+
+
+    public static void main(String[] args) throws IOException {
+
         draw();
 
     }
